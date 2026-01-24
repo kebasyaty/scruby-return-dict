42b8edbed6b154bc86750e39710bc64d253dc25f
kebasyaty <kebasyaty@gmail.com>
2026-01-24 13:51:29 +0200

    add a Copyright comments

--- src/scruby_return_dict/__init__.py
+++ src/scruby_return_dict/__init__.py
@@ -1,1 +1,3 @@
-"""???"""
+# Copyright (c) 2026 Gennady Kostyunin
+# SPDX-License-Identifier: MIT
+"""Scruby-Return-Dict - In search methods, returns results in the form of dictionaries."""
--- src/scruby_return_dict/plugin.py
+++ src/scruby_return_dict/plugin.py
@@ -1,1 +1,20 @@
-"""???"""
+# Scruby-Return-Dict - In search methods, returns results in the form of dictionaries.
+# Copyright (c) 2026 Gennady Kostyunin
+# SPDX-License-Identifier: MIT
+"""Scruby-Return-Dict Plugin."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from scruby_plugin import ScrubyPlugin
+
+
+class ReturnDict(ScrubyPlugin):
+    """Scruby-Return-Dict Plugin.
+
+    In search methods, returns results in the form of dictionaries.
+    """
+
+    def __init__(self, scruby_self: Any) -> None:  # noqa: D107
+        ScrubyPlugin.__init__(self, scruby_self)
